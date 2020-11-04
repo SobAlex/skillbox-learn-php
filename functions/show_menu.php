ccea20a8c662e19b30fb54c4f6a905b4be5b085d
Sobolev Aleksandr <sob-al@mail.ru>
2020-11-04 18:45:48 +0500

    осталось разработать функции cutSring и arraySort

--- functions/show_menu.php
+++ functions/show_menu.php
@@ -2,16 +2,25 @@
 
 // производим какие-то действия над контентом. в данном случае преобразуем меню
 
+include $_SERVER['DOCUMENT_ROOT'] . '/includes/main_menu.php'; // подключаем массив меню
+
+// сортировка меню
+function arraySort($array, $key = 'sort', $sort = SORT_ASC): array
+{
+}
+
+// обрезка строки
 function cutString($array, $length = 12, $appends = '...'): string
 {
 }
 
-function arraySort(array $array, $key = 'sort', $sort = SORT_ASC): array
+function showMenu($mainMenu)
 {
+    arraySort($mainMenu); // вызов сортировки
+    cutString($mainMenu); // вызов обрезки
+
+    include $_SERVER['DOCUMENT_ROOT'] . '/templates/menu.php';     // подключаем разметку меню
+
 }
 
-function showMenu($classMenu)
-{
-    include $_SERVER['DOCUMENT_ROOT'] . '/includes/main_menu.php'; // подключаем массив меню
-    include $_SERVER['DOCUMENT_ROOT'] . '/templates/menu.php';     // подключаем разметку меню
-}
+showMenu($mainMenu); // вывод готового меню гдето в разметке
--- route/news/index.php
+++ route/news/index.php
@@ -3,11 +3,14 @@
 include $_SERVER['DOCUMENT_ROOT'] . '/includes/main_menu.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/templates/header.php';
 
-?>
+for ($i = 0; $i < count($mainMenu); $i++) {
 
-<h1><?= $mainMenu[3]['title'] ?></h1>
+?>
+    <h1><?= $mainMenu[$i]['title'] ?></h1>
 
 <?php
+
+}
 
 include $_SERVER['DOCUMENT_ROOT'] . '/templates/footer.php';
 
